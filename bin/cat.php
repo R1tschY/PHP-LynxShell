bfc328216cb0e58544ee79959b05349e33038d24
Richard Liebscher <richard.liebscher@googlemail.com>
2012-05-24 01:15:52 +0200

    correct output channel

--- bin/cat.php
+++ bin/cat.php
@@ -1,14 +1,14 @@
 <?php
 
 if (trim($args[1]) == '') {
-  Answer::addOutput('stdout', $args[0].': No input file');
+  Answer::addOutput('o', $args[0].': No input file');
   return ;
 }
 
 if (file_exists($args[1])) {
-  Answer::addOutput('stdout', file_get_contents($args[1]));
+  Answer::addOutput('o', file_get_contents($args[1]));
 } else {
-  Answer::addOutput('stdout', $args[0].': '.$args[1].': No such file or directory');
+  Answer::addOutput('o', $args[0].': '.$args[1].': No such file or directory');
 }
 
 ?>
